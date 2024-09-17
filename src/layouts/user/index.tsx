93973c06e994b41c56cb975865d63a79aae989af
Bruno Caitano <snowrunescape@snowdev.com.br>
2024-09-16 20:30:31 -0400

    fix: aside

--- src/layouts/user/index.tsx
+++ src/layouts/user/index.tsx
@@ -1,4 +1,3 @@
-
 import Menu from "@/components/layout/Menu";
 import Profile from "@/components/layout/Profile";
 import { Outlet } from "react-router-dom";
@@ -9,20 +8,19 @@ const User = () => {
 
   return (
     <div className="flex bg-lilac min-h-screen">
-      <div className="flex flex-col justify-between gap-5 p-5 w-[250px]">
-        <div className="flex flex-col gap-5">
-          <div>
-            <img
-              src="/assets/img/logo.png"
-              width={120}
-            />
+      <aside className="h-screen sticky top-0 w-[250px]">
+        <div className="min-h-screen flex flex-col justify-between gap-5 p-5">
+          <div className="flex flex-col gap-5">
+            <div>
+              <img src="/assets/img/logo.png" width={120} />
+            </div>
+
+            <Menu menus={menus} />
           </div>
 
-          <Menu menus={menus} />
+          <Profile />
         </div>
-
-        <Profile />
-      </div>
+      </aside>
 
       <div className="px-8 py-5 flex-1 bg-[#f0eff6]">
         <Outlet />
